992c925c678271775efe10610cf0ddb4eef2c593
tiwari akshat <tiwaiakshat240@gmail.com>
2026-03-05 14:12:58 +0530

    implemented the chages here

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASEURL || 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_BASEURL,
 });
 
 export default api;
